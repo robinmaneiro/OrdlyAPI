6baa168985e0b2f45e7cd38d04cd2d003a6fcb5d
Robin Maneiro <robin.maneiro@gmail.com>
2025-10-26 11:49:37 +0000

    Renamed 'TokenManager' to be 'SessionManager'

--- src/main/kotlin/com/robinmaneiro/order_kiosk_backend/account/controller/AccountController.kt
+++ src/main/kotlin/com/robinmaneiro/order_kiosk_backend/account/controller/AccountController.kt
@@ -2,6 +2,7 @@ package com.robinmaneiro.order_kiosk_backend.account.controller
 
 import com.robinmaneiro.order_kiosk_backend.account.service.AccountService
 import com.robinmaneiro.order_kiosk_backend.account.service.model.AccountDetailsResponse
+import com.robinmaneiro.order_kiosk_backend.auth.service.AuthService
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
@@ -9,7 +10,8 @@ import org.springframework.web.bind.annotation.RestController
 @RestController
 @RequestMapping("/api/v1/users")
 class AccountController(
-    private val accountService: AccountService
+    private val accountService: AccountService,
+    private val authService: AuthService
 ) {
     @GetMapping("/me")
     fun getAccountDetails(): AccountDetailsResponse {
